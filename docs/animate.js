ee8231118e4c6c104dc9377a7d4ec9933e3815b9
yugantmaurya <yugantmaurya1@gmail.com>
2026-06-02 21:41:04 +0530

    Added animeted nav

---
+++ docs/animate.js
@@ -0,0 +1,9 @@
+function showslidebar(){
+    const slidebar = document.querySelector('.slidebar')
+    slidebar.style.display = 'flex'
+}
+
+function hideslidebar(){
+    const slidebar = document.querySelector('.slidebar')
+    slidebar.style.display = 'none'
+}
